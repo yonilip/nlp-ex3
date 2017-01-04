841253e2f13f3046303095ac3fc8053fbd5f8a8a
Yuval Globerson <yuvalg@cs.huji.ac.il>
2017-01-04 14:01:25 +0200

    dot-product and stiff

--- gradient.py
+++ gradient.py
@@ -3,8 +3,18 @@ from nltk.corpus import treebank
 from scipy.optimize import fmin_bfgs
 from features import make_feature_function
 import numpy
+import random
 
 NUM_FEATURES = 100
+DEBUG = True
+
+
+def debug_print(s, nl=True):
+    if DEBUG:
+        print s,
+    if nl:
+        print
+
 
 sents = treebank.tagged_sents()
 with open("features.txt") as features_file:
@@ -16,16 +26,12 @@ for sent in sents:
         TAGS.add(tag)
 
 
-def dot_product(x,y):
-    return sum([a*b for a, b in zip(x,y)])
-
-
 def Z(words, prev_tag, index, w):
     return sum([score(words, prev_tag, other_tag, index, w) for other_tag in TAGS])
 
 
 def score(words, prev_tag, tag, index, w):
-    return exp(dot_product(f(words, prev_tag, tag, index), w))
+    return exp(numpy.dot(f(words, prev_tag, tag, index), w))
 
 
 # def f(words, prev_tag, tag, index):
@@ -36,12 +42,13 @@ def gradient(w):
     """
     grad = [0] * NUM_FEATURES
 
+    debug_print("Preprocessing for gradient...")
     # preprocess Z,f values
     z_values = {}
     f_values = {}
     for i, sent in enumerate(sents):
         if i % 10 == 0:
-            print i
+            debug_print(i)
         words = [word for word, tag in sent]
         for j in xrange(len(sent)):
             z_all = 0
@@ -50,9 +57,10 @@ def gradient(w):
                 z_all += z_values[(i,j,tag)]
                 f_values[(i,j,tag)] = f(words, None, tag, j)
             z_values[(i,j,"ALL_TAGS_SUM")] = z_all
-    print "Done pre-processing"
+    debug_print("Done pre-processing, compute partial derivatives")
 
     for k in xrange(NUM_FEATURES):
+        debug_print(k)
         grad_k = 0
         for i,sent in enumerate(sents):
             for j in xrange(len(sent)):
@@ -64,17 +72,20 @@ def gradient(w):
                 z_grad /= z_values[(i,j,"ALL_TAGS_SUM")]
                 grad_k += f_k - z_grad
         grad[k] = grad_k
+    debug_print("Done computing gradient")
     return grad
 
 
 def minus_log_likelihood(w):
+    debug_print("Compute likelihood...")
     ll = 0
     for sent in sents:
         words = [word for word, tag in sent]
         for j in xrange(len(sent)):
             true_tag = sent[j][1]
-            ll += dot_product(f(words, None, true_tag, j), w)
+            ll += numpy.dot(f(words, None, true_tag, j), w)
             ll -= log(Z(words, None, j, w))
+    debug_print("Done")
     return -1 * ll
 
 
@@ -82,8 +93,11 @@ def minus_gradient(w):
     return -1 * numpy.array(gradient(w))
 
 
-def optimize():
-    best_w = fmin_bfgs(minus_log_likelihood, [1] * NUM_FEATURES, fprime=minus_gradient)
+def optimize(x0=None):
+    if x0 is None:
+        x0 = [random.random() - 0.5 for i in xrange(NUM_FEATURES)]
+        print "Initial guess is", x0
+    best_w = fmin_bfgs(minus_log_likelihood, x0, fprime=minus_gradient)
     with open("results.txt", "w") as res_file:
         res_file.write(str(best_w))
     return best_w
